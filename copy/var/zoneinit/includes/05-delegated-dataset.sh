a4fcc1636cf9e653f491b97df718cfd755ac07f7
Thomas Merkel <tm@core.io>
2015-02-13 15:10:08 +0100

    enable the services

---
+++ copy/var/zoneinit/includes/05-delegated-dataset.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+UUID=$(mdata-get sdc:uuid)
+DDS=zones/${UUID}/data
+
+if zfs list ${DDS} 1>/dev/null 2>&1; then
+	zfs create ${DDS}/www   || true
+
+	zfs set mountpoint=/var/www ${DDS}/www
+fi
