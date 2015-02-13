5e70d8c1b826fc9e1823ddea180e7fc6f56f4d62
Thomas Merkel <tm@core.io>
2015-02-13 15:47:40 +0100

    update hostname if possible

--- copy/var/zoneinit/includes/45-nginx.sh
+++ copy/var/zoneinit/includes/45-nginx.sh
@@ -1,1 +1,10 @@
+# Generate hostname for nginx config
+
+if mdata-get sdc:hostname 1>/dev/null 2>&1; then
+	host=$(mdata-get sdc:hostname)
+	/opt/local/bin/sed -i "s:server_name.*;:server_name ${host};:g" \
+		/opt/local/etc/nginx/nginx.conf
+fi
+
+# Enable nginx
 /usr/sbin/svcadm enable svc:/pkgsrc/nginx:default
